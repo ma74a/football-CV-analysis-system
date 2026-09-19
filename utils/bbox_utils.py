8c9715138c5abf5da7c473f6bed9aa406702e8d0
mahmoud etman <metman835@gmail.com>
2026-09-20 00:26:32 +0300

    add measuer distance function

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -1,4 +1,4 @@
-
+import math
 
 def get_bbox_conter(bbox):
     x1, y1, x2, y2 = bbox
@@ -7,3 +7,6 @@ def get_bbox_conter(bbox):
 def get_bbox_width(bbox):
     x1, y1, x2, y2 = bbox 
     return int(x2 - x1)
+
+def measure_distance(pt1, pt2):
+    return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
