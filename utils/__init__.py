8de03e365e446a68321269c18031181f598a585f
mahmoud etman <metman835@gmail.com>
2026-09-24 19:43:48 +0300

    updating __init__.py file

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,5 +2,6 @@ from .video_utils import read_video, save_video
 from .bbox_utils import (
     get_bbox_conter,
     get_bbox_width,
-    measure_distance
+    measure_distance,
+    measure_xy_distance
 )
