ef5a759e7e233d8b56c8fd132fb44dfd42bd4cff
mahmoud etman <metman835@gmail.com>
2026-09-24 19:43:30 +0300

    adding function to measuer between to points

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -10,3 +10,9 @@ def get_bbox_width(bbox):
 
 def measure_distance(pt1, pt2):
     return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
+
+
+def measure_xy_distance(pt1, pt2):
+    x_dist = pt1[0] - pt2[0]  # old_x - new_x
+    y_dist = pt1[1] - pt2[1]  # old_y - new_y
+    return x_dist, y_dist
